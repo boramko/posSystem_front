3bd93e972b925aace673c6e886e8a769429403dd
boramko <darkarm3@naver.com>
2023-10-10 17:30:42 +0900

    라우터 관리부분 수정

--- src/ProtectedRouter.tsx
+++ src/ProtectedRouter.tsx
@@ -1,40 +1,28 @@
-import axiosInstance from "@axiosInstance";
-import React, {useEffect, useState} from "react";
-import {useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
     children: React.ReactNode;
-}
-const ProtectedRouter: React.FC<Props> = ({ children }) => {
-    const navi = useNavigate();
+};
 
-    // const checkToken = (token:String) => {
-    //     let isToken = false;
- 
-    //     if(token != null){
-    //         axiosInstance.post("/checkToken",{
-    //         }).then(data => {
-    //             console.log(data);
-    //             isToken = true;
-    //         }).catch(error => {
-    //             console.log(error);
-    //             isToken = false;
-    //         });
-    //     }
-    //     return isToken;
-    // }
+const ProtectedRouter: React.FC<Props> = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // 상태 추가
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const token = localStorage.getItem('token') || null;
-        
-        // checkToken(token);
+        const token = localStorage.getItem('token');
+
+        // 로컬 스토리지에 토큰이 있는지 확인하고, 그 결과를 상태에 설정합니다.
+        setIsAuthenticated(!!token);
+
+        // 토큰이 없으면 로그인 페이지로 리다이렉트합니다.
         if (!token) {
-            navi('/login');
+            navigate('/login');
         }
-    }, [navi]);
+    }, [navigate]);
 
-    return <>{children}</>
-}
+    // isAuthenticated가 true일 때만 자식 컴포넌트를 렌더링합니다.
+    return <>{isAuthenticated && children}</>;
+};
 
-export default ProtectedRouter
-
+export default ProtectedRouter;
